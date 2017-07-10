6755cb6c724faffe67fda9533dff55ca8497868a
Wilfred Hughes <whughes@ahl.com>
2017-07-10 11:50:04 +0100

    Run prettier

--- config.js
+++ config.js
@@ -1,15 +1,12 @@
-'use strict';
+"use strict";
 
-var _ = require('underscore');
-var path = require('path');
+var _ = require("underscore");
+var path = require("path");
 
-var etc = require('etc');
+var etc = require("etc");
 
-var configPath = path.join(require('app-root-dir').get(), 'servicenow.yaml');
-var config = etc()
-    .use(require('etc-yaml'))
-    .file(configPath)
-    .toJSON();
+var configPath = path.join(require("app-root-dir").get(), "servicenow.yaml");
+var config = etc().use(require("etc-yaml")).file(configPath).toJSON();
 
 // 'CR1234' -> 'CR'
 function getPrefix(recordId) {
@@ -23,9 +20,11 @@ function tableAttr(recordIdOrPrefix, key, defaultValue) {
   defaultValue = defaultValue || null;
 
   var prefix = getPrefix(recordIdOrPrefix);
-  return _.find(config.tables, function(table) {
-    return table.prefix == prefix;
-  })[key] || defaultValue;
+  return (
+    _.find(config.tables, function(table) {
+      return table.prefix == prefix;
+    })[key] || defaultValue
+  );
 }
 
 function prefixFromTableName(tableName) {
@@ -35,19 +34,19 @@ function prefixFromTableName(tableName) {
 }
 
 function tableName(recordIdOrPrefix) {
-  return tableAttr(recordIdOrPrefix, 'name');
+  return tableAttr(recordIdOrPrefix, "name");
 }
 
 function dateField(recordIdOrPrefix) {
-  return tableAttr(recordIdOrPrefix, 'date_field', 'sys_created_on');
+  return tableAttr(recordIdOrPrefix, "date_field", "sys_created_on");
 }
 
 function cloneFields(recordIdOrPrefix) {
-  return tableAttr(recordIdOrPrefix, 'clone_fields');
+  return tableAttr(recordIdOrPrefix, "clone_fields");
 }
 
 function webFields(recordIdOrPrefix) {
-  return tableAttr(recordIdOrPrefix, 'web_fields');
+  return tableAttr(recordIdOrPrefix, "web_fields");
 }
 
 module.exports = {
